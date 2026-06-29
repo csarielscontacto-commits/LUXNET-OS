003778f1165d7d4e95a3f58da1857241613d3c95
Juana Hernández cortez <csarielscontacto@gmail.com>
2026-06-29 00:35:30 -0600

    Create _layout.tsx

---
+++ _layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: "fade",
+          contentStyle: {
+            backgroundColor: "#050816",
+          },
+        }}
+      />
+    </>
+  );
+}
